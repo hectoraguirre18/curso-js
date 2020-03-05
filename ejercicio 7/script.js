5a0c72b6fe6d13020ec1d9b027cd3282c082bdce
Hector <a320793@uach.mx>
2020-03-05 10:39:07 -0700

    ejercicio 7

---
+++ ejercicio 7/script.js
@@ -0,0 +1,12 @@
+function onFinish() {
+  let name = document.getElementById("name").value;
+  let gender = document.getElementById("gender").value;
+  greet(name, gender);
+}
+
+function greet(name, gender) {
+  document.getElementById("message").innerHTML = `Bienvenid${gender == "male" 
+    ? "o" : gender == "female"
+    ? "a" : "e"} ${name}! n.n`;
+}
+
